362c7451f810d66ab2dcc3f1a6672f240a38cb13
Mustakim Khondaker <thisismustakim@gmail.com>
2025-03-01 13:22:58 +0600

    init

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import SwaggerUI from "swagger-ui-react";
+import "swagger-ui-react/swagger-ui.css";
+import "./App.css"; // Import the CSS file
+
+function App() {
+  return (
+    <>
+      <nav className="navbar">
+        <img className="logo" src="rnr_rooms_logo_white.svg" alt="logo" />
+        <h3>ETG API Specification</h3>
+      </nav>
+      <SwaggerUI url="./openapi.yaml" />
+    </>
+  );
+}
+
+export default App;
